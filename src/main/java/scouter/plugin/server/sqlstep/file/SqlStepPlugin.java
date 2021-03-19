95e8056bcfef351c700e6ffa59c2362c562e6ab0
yosong.heo <yosong.heo@gmail.com>
2021-03-19 22:15:11 +0900

    :zap: url info adding

--- src/main/java/scouter/plugin/server/sqlstep/file/SqlStepPlugin.java
+++ src/main/java/scouter/plugin/server/sqlstep/file/SqlStepPlugin.java
@@ -121,6 +121,7 @@ public class SqlStepPlugin {
         try {
             xlogLogger.execute(ServiceLogging.builder()
                     .name(op.objName)
+                    .url(this.getString(helper.getServiceString(p.service)))
                     .requestTime(this.dateTimeFormatter.format(new Date(p.endTime - p.elapsed).toInstant()))
                     .elapsed(p.elapsed)
                     .txid(Hexa32.toString32(p.txid))
