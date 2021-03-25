2863104c889beef67cbcdf9961671834ba35422d
yosong.heo <yosong.heo@gmail.com>
2021-03-25 19:11:56 +0900

    sql query history data changed

--- src/main/java/scouter/plugin/server/sqlstep/file/SqlStepPlugin.java
+++ src/main/java/scouter/plugin/server/sqlstep/file/SqlStepPlugin.java
@@ -125,7 +125,7 @@ public class SqlStepPlugin {
                         .name(op.objName)
                         .url(this.getString(helper.getServiceString(p.service)))
                         .requestTime(this.dateTimeFormatter.format(new Date(p.endTime - p.elapsed).toInstant()))
-                        .error(helper.getErrorString(p.error))
+                        .error(StringUtil.emptyToDefault(jh.sqlError,""))
                         .txid(Hexa32.toString32(p.txid))
                         .gxid(p.gxid != 0 ? Hexa32.toString32(p.gxid) : null)
                         .history(jh)
